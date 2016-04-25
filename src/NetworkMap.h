a244d3c51206142d985f5100c7bf9905d8452da7
dcepa95 <dcepa95@gmail.com>
2016-04-25 23:32:47 +0100

    Doxy

--- src/NetworkMap.h
+++ src/NetworkMap.h
@@ -23,45 +23,170 @@ using namespace std;
 
 #define WIN_WIDTH 3000
 #define WIN_HEIGHT 3000
-
+/**
+ * Classe que lê e trata de toda a informação relativa as linhas.
+ */
 class NetworkMap {
 
 	Graph<Stop> map;
 
 public:
-	//Constructors
+	/**
+	 * Construtor, inicializa o varial map como um grafo vazio.
+	 */
 	NetworkMap();
 
-	//Getters
+	/**
+	 * Retorna o grafo com a informação das linhas.
+	 *
+	 * @return Graph<Stop> grafo com a respetiva informação.
+	 */
 	Graph<Stop> getMap() const;
 
-	//Setters
+	/**
+	 * Iguala o grafo a um novo passado como argumento.
+	 *
+	 * @param m grafo novo.
+	 */
 	void setMap(Graph<Stop> m);
 
 	//Methods
+
+	/**
+	 * Le o ficheiro passado como argumento e processa a informação para o grafo.
+	 *
+	 * @param filepath caminho para o ficheiro.
+	 * @return bool true se é possível ler, false case não seja.
+	 */
 	bool loadMap(string filepath);
+
+	/**
+	 * Cria as arestas do grafo.
+	 */
 	void setConnections();
+	/**
+	 * Imprime o grafo na consola.
+	 */
 	void displayMap();
 
+	/**
+	 * Calcula o peso total entre duas paragens.
+	 *
+	 * @param s1 paragem 1.
+	 * @param s2 paragem 2.
+	 * @return float peso total das arestas entre as duas paragens.
+	 */
 	float pathWeight(const Stop& s1, const Stop &s2);
+	/**
+	 * Calcula o caminho entre duas paragens com peso menor.
+	 *
+	 * @param s1 Codigo da paragem 1.
+	 * @param s2 Codigo da paragem 2.
+	 * @param weight peso total
+	 * @return vector<Stop> vetor com todas as paragens do caminho com menor peso.
+	 */
 	vector<Stop> findLightestPath(string s1, string s2, float& weight);
+	/**
+	 * Calcula o caminho mais rápido entre duas paragens.
+	 *
+	 * @param s1 Codigo da paragem 1.
+	 * @param s2 Codigo da paragem 2.
+	 */
 	void findFastestPath(string s1, string s2);
+	/**
+	 * Calcula o caminho mais barato entre duas paragens.
+	 *
+	 * @param s1 Codigo da paragem 1.
+	 * @param s2 Codigo da paragem 2.
+	 */
 	void findCheapestPath(string s1, string s2);
+	/**
+	 * Calcula o caminho mais curto entre duas paragens.
+	 *
+	 * @param s1 Codigo da paragem 1.
+	 * @param s2 Codigo da paragem 2.
+	 */
 	void findShortestPath(string s1, string s2);
+	/**
+	 * Calcula o caminho com menos trocas de veiculo entre duas paragens.
+	 *
+	 * @param s1 Codigo da paragem 1.
+	 * @param s2 Codigo da paragem 2.
+	 */
 	void findLeastLineSwitchesPath(string s1, string s2);
 };
 
+/**
+ * Verifica se o vetor com dado elemento
+ *
+ * @param v vetor
+ * @param s elemento
+ * @return bool true caso contenha,false caso nao encontre.
+ */
 bool exists_in_vector(vector<string> v, string s);
+/**
+ * Imprime no elemento NetworkMap num interface grafica
+ */
 void graphView(NetworkMap nm);
+/**
+ * Procura por um vertice num vetor de vertices
+ *
+ * @param vertexSet vetor
+ * @param to_find vertice a encontrar
+ * @return int indice do vertice no vetor, -1 caso nao exista
+ */
 int findVertexInVector(vector <Vertex<Stop>*> vertexSet, Vertex<Stop>* to_find);
+/**
+ * Atualiza o peso dos vertices de um NetworkMap com uma função passada como argumento
+ *
+ * @param weightFunction funçao para calcular o peso dos vertices
+ * @param nm NetworkMap a ser atualizado
+ */
 void resetEdges(float(*weightFunction)(const Stop&, const Stop&), NetworkMap& nm);
+/**
+ * Calcula o preço de uma viagem com base no numero de zonas em que esta passa.
+ *
+ * @param numZones numero de zonas
+ * @return float preço
+ */
 float calcPrice(int numZones);
 
 // Methods to calculate edge weight
+/**
+ *
+ */
 float testWeight(const Stop& s1, const Stop& s2);
+/**
+ * Calcula o peso entre a paragem s1 e s2 com base no tempo a que se demora a chegar de s1 a s2.
+ *
+ * @param s1 paragem 1
+ * @param s2 paragem 2
+ * @return float peso da aresta em termos de tempo.
+ */
 float timeWeight(const Stop& s1, const Stop& s2);
+/**
+ * Calcula o peso entre a paragem s1 e s2 com base no preço que se paga ao viajar de s1 ate s2.
+ *
+ * @param s1 paragem 1
+ * @param s2 paragem 2
+ * @return float peso da aresta em termos de tpreço.
+ */
 float priceWeight(const Stop& s1, const Stop& s2);
+/**
+ * Calcula o peso entre a paragem s1 e s2 com base na distancia entre s1 e s2.
+ *
+ * @param s1 paragem 1
+ * @param s2 paragem 2
+ * @return float peso da aresta em termos de distancia.
+ */
 float distanceWeight(const Stop& s1, const Stop& s2);
+/**
+ * Calcula o peso entre a paragem s1 e s2 com base no numero de trocas a que se efetua para ir de s1 a s2.
+ *
+ * @param s1 paragem 1
+ * @param s2 paragem 2
+ * @return float peso da aresta em termos de trocas.
+ */
 float lineSwitchWeight(const Stop&s1, const Stop& s2);
 
 #endif /* SRC_NETWORKMAP_H_ */
--- src/Stop.h
+++ src/Stop.h
@@ -16,7 +16,9 @@ using namespace std;
 
 #define LAT 0
 #define LON 1
-
+/**
+ * Classe relativa a uma paragem
+ */
 class Stop {
 
 	string name;
@@ -25,38 +27,167 @@ class Stop {
 	int wait_time;
 
 public:
-	//Constructors
+	/**
+	 * Construtor, inicializa uma paragem vazia.
+	 */
 	Stop();
+	/**
+	 * Construtor, incializa uma paragem.
+	 *
+	 * @param n nome
+	 * @param lat latitude
+	 * @param lon longitude
+	 * @param time frequencia de passagem do transporte
+	 * @param nod codigo da paragem
+	 * @param zon zona a que a paragem pertence
+	 */
 	Stop(string n, float lat, float lon, int time, string nod, string zon);
+	/**
+	 * Construtor, inicializa uma paragem vazia com nome n.
+	 *
+	 * @param n nome da paragem
+	 */
 	Stop(string n);
 
 	//Getters
+	/**
+	 * Devolve o nome da paragem
+	 *
+	 * @return string nome
+	 */
 	string getName() const;
+	/**
+	 * Devolve a latitude da paragem
+	 *
+	 * @return float latitude
+	 */
 	float getLatitude() const;
+	/**
+	 * Devolve a longitude da paragem
+	 *
+	 * @return float longitude
+	 */
 	float getLongitude() const;
+	/**
+	 * Devolve o codigo da paragem
+	 *
+	 * @return string codigo
+	 */
 	string getNode() const;
+	/**
+	 * Devolve a zona da paragem
+	 *
+	 * @return string zona
+	 */
 	string getZone() const;
+	/**
+	 * Devolve o tempo de espera na paragem
+	 *
+	 * @return string codigo
+	 */
 	int getWaitTime() const;
+	/**
+	 * Devolve a linha da paragem
+	 *
+	 * @return string linha
+	 */
 	string getLine() const;
 
 	//Setters
+	/**
+	 * Muda o nome da paragem.
+	 *
+	 * @param n novo nome.
+	 */
 	void setName(string n);
+	/**
+	 * Muda as coordenadas geograficas da paragem.
+	 *
+	 * @param lat nova latitude
+	 * @param lon nova longitude
+	 */
 	void setCoords(float lat, float lon);
+	/**
+	 * Muda a latitude da paragem
+	 *
+	 * @param lat nova latitude
+	 */
 	void setLatitude(float lat);
+	/**
+	 * Muda a longitude da paragem
+	 *
+	 * @param lon nova longitude
+	 */
 	void setLongitude(float lon);
+	/**
+	 * Muda o codigo da paragem.
+	 *
+	 * @param p novo codigo
+	 */
 	void setNode(string p);
+	/**
+	 * Muda a zona da paragem.
+	 *
+	 * @param zon nova zona
+	 */
 	void setZone(string zon);
+	/**
+	 * Muda o tempo de espera da paragem.
+	 *
+	 * @param waitTime novo tempo de espera
+	 */
 	void setWaitTime(int waitTime);
 
 	//Methods
+	/**
+	 * Calcula a distancia da paragem atual a uma outra paragem.
+	 *
+	 * @param s paragem a qual se quer saber a distancia.
+	 * @return float distancia entre as duas paragens.
+	 */
 	float calcDistance(Stop s) const;
+	/**
+	 * Calcula a distancia da paragem atual até um ponto geografico
+	 *
+	 * @param lat latitude do ponto
+	 * @param lon longitudo do ponto
+	 * @return float distancia entre a paragem e o ponto
+	 */
 	float calcDistance(float lat, float lon);
+	/**
+	 * Calcula o tempo que o veiculo demora a chegar da paragem atual a uma outra paragem
+	 *
+	 * @param s paragem a qual se quer saber o tempo que se demora chegar
+	 * @param velocity velocidade do veiculo
+	 * @return float tempo que o veiculo demora a ir da paragem atual a paragem destino.
+	 */
 	float calcTimeBetween(const Stop& s, int velocity) const;
+	/**
+	 * Calcula o tempo que o veiculo demora a chegar da paragem atual a um ponto geografico
+	 *
+	 * @param lat latitude do ponto
+	 * @param lon longitude do pont
+	 * @param velocity velocidade do veiculo
+	 * @return float tempo que o veiculo demora a ir da paragem atual ao ponto.
+	 */
 	float calcTimeBetween(float lat, float lon, int velocity);
 
 	//Operator Overloading
+	/**
+	 * Overloading do operador==
+	 *
+	 * @return true se o nome de s1 é igual ao de s2, false caso contrario.
+	 */
 	friend bool operator==(const Stop& s1, const Stop &s2);
+	/**
+	 * Overloading do operador==
+	 *
+	 * @return true se o nome de s1 não é igual ao de s2, false caso contrario.
+	 */
 	friend bool operator!= (const Stop& s1, const Stop &s2);
+	/**
+	 * Overloading do operador << para que se possa imprimir um elemento do tipo Stop
+	 */
 	void operator<< (ostream &o);
 
 };
